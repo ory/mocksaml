5ce0cd62a45145b7bb2b81eea0eecc815e9a0ee9
Deepak Prabhakara <deepak.prabhakara@ory.sh>
2025-12-11 11:55:35 +0000

    WIP

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  darkMode: 'class',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  // plugins: [require('daisyui')],
-  // daisyui: {
-  //   themes: ['light'],
-  // },
-};
